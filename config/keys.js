8a6c73ee1c9ff2561604680ee876a7b22b58a802
mariana-k <mariana.kyrkosh@gmail.com>
2021-05-02 22:32:13 +0300

    Updates

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
-modules.export = {
-    mongoURI: 'mongodb+srv://admin:admin@cluster0.lav2h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+module.exports = {
+    mongoURI: 'mongodb+srv://admin:admin@cluster0.lav2h.mongodb.net/mern_shopping?retryWrites=true&w=majority'
 }
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const items = require('./routes/api/items');
 
 const app = express();
 
@@ -12,6 +15,14 @@ mongoose.connect(db)
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
+app.use('/api/items', items);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
